aae8c926fc05da0b0caa973f1c63fe199b53fcc0
Misha Moroshko <michael.moroshko@gmail.com>
2020-05-26 07:53:25 +1000

    Add Container usage

--- website/src/layouts/Usage.js
+++ website/src/layouts/Usage.js
@@ -3,13 +3,55 @@ import PropTypes from "prop-types";
 import { MDXProvider } from "@mdx-js/react";
 import { Link as GatsbyLink } from "gatsby";
 import * as allDesignSystem from "basis";
+import { paramCase } from "param-case";
 
-const { BasisProvider, defaultTheme, Container } = allDesignSystem;
+const {
+  BasisProvider,
+  defaultTheme: theme,
+  Link,
+  Container,
+  Text,
+  List,
+} = allDesignSystem;
+const components = {
+  ...allDesignSystem,
+  a: (props) => <Link newTab={false} {...props} />,
+  p: (props) => <Text margin="0 0 4 0" {...props} />,
+  h3: ({ children }) => {
+    console.log(paramCase(children));
+    return (
+      <Text as="h3" textStyle="heading4" margin="0 0 4 0">
+        {children}
+      </Text>
+    );
+  },
+  ul: ({ children }) => (
+    <List>
+      {children.map((child, index) => (
+        <List.Item key={index}>{child.props.children}</List.Item>
+      ))}
+    </List>
+  ),
+  inlineCode: (props) => (
+    <code
+      css={{
+        backgroundColor: theme.colors.secondary.purple.t30,
+        padding: "0 2px",
+      }}
+      {...props}
+    />
+  ),
+  Color: ({ children }) => (
+    <Container bg={children} padding="0 2" width="200">
+      <Text>{children}</Text>
+    </Container>
+  ),
+};
 
 function Usage({ children }) {
   return (
-    <BasisProvider theme={defaultTheme} InternalLink={GatsbyLink}>
-      <MDXProvider components={allDesignSystem}>
+    <BasisProvider theme={theme} InternalLink={GatsbyLink}>
+      <MDXProvider components={components}>
         <Container padding="3 6">{children}</Container>
       </MDXProvider>
     </BasisProvider>
