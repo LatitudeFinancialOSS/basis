f75b77915cbb10ff6da1e0758aea0eed13691bdf
Misha Moroshko <michael.moroshko@gmail.com>
2020-06-18 08:57:49 +1000

    Refactor Flex kitchen sink

--- website/src/components/kitchen-sink/Flex.js
+++ website/src/components/kitchen-sink/Flex.js
@@ -5,20 +5,20 @@ import KitchenSinkLayout from "./KitchenSinkLayout";
 function KitchenSinkFlex() {
   return (
     <KitchenSinkLayout name="Flex">
-      <Container padding="4" height="160">
-        <Flex height="100%" placeItems="top center">
+      <Container padding="4">
+        <Flex height="128" placeItems="top center">
           <Button>top center</Button>
         </Flex>
       </Container>
 
-      <Container padding="4" height="160" bg="grey.t05">
-        <Flex height="100%" placeItems="center right">
+      <Container padding="4" bg="grey.t05">
+        <Flex height="128" placeItems="center right">
           <Button>center right</Button>
         </Flex>
       </Container>
 
-      <Container padding="4" height="160">
-        <Flex height="100%" placeItems="center">
+      <Container padding="4">
+        <Flex height="128" placeItems="center">
           <Button>center</Button>
         </Flex>
       </Container>
