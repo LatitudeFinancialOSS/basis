56e47b2ee91bc8ea5569138978a7d50a75d288e8
Misha Moroshko <michael.moroshko@gmail.com>
2020-08-25 09:32:49 +1000

    Remove async

--- website/src/pages/playground/index.test.js
+++ website/src/pages/playground/index.test.js
@@ -5,7 +5,7 @@ import "@testing-library/jest-dom/extend-expect";
 import Playground from "./index";
 
 describe("Playground", () => {
-  it("loads successfully", async () => {
+  it("loads successfully", () => {
     expect(() => {
       render(
         <RecoilRoot>
