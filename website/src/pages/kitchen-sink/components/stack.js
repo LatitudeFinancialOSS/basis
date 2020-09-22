440380ced958b073e16bd7f09f39ae8ecf8629a0
Misha Moroshko <michael.moroshko@gmail.com>
2020-09-22 21:01:33 +1000

    Hide empty Stack children

--- website/src/pages/kitchen-sink/components/stack.js
+++ website/src/pages/kitchen-sink/components/stack.js
@@ -1,6 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
 import { Container, Stack, Text, Placeholder } from "basis";
 import KitchenSinkLayout from "../../../components/kitchen-sink/KitchenSinkLayout";
+
+function MaybePlaceholder({ label, width, height }) {
+  if (label === "2" || label === "5") {
+    return null;
+  }
+
+  return <Placeholder label={label} width={width} height={height} />;
+}
+
+MaybePlaceholder.propTypes = {
+  label: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+};
 
 function KitchenSinkStack() {
   return (
@@ -157,6 +172,39 @@ function KitchenSinkStack() {
             </Container>
           </Stack>
         </Container>
+      </Container>
+
+      <Container padding="4">
+        <Text textStyle="heading4" margin="0 0 4 0">
+          With empty children (2 and 5)
+        </Text>
+        <Stack direction="horizontal" gap="8">
+          <Container bg="secondary.lightBlue.t25" width="280">
+            <Stack gap="4">
+              <MaybePlaceholder label="1" height="40" width="100" />
+              <MaybePlaceholder label="2" height="40" width="100" />
+              {null}
+              <MaybePlaceholder label="3" height="40" width="100" />
+              {undefined}
+              {1 + 1 === 2 && (
+                <MaybePlaceholder label="4" height="40" width="100" />
+              )}
+              <MaybePlaceholder label="5" height="40" width="100" />
+              <MaybePlaceholder label="6" height="40" width="100" />
+            </Stack>
+          </Container>
+          <Container bg="secondary.lightBlue.t25" width="592">
+            <Stack direction="horizontal" gap="2">
+              <MaybePlaceholder label="1" height="40" width="100" />
+              <MaybePlaceholder label="2" height="40" width="100" />
+              <MaybePlaceholder label="3" height="40" width="100" />
+              {false}
+              <MaybePlaceholder label="4" height="40" width="100" />
+              <MaybePlaceholder label="5" height="40" width="100" />
+              <MaybePlaceholder label="6" height="40" width="100" />
+            </Stack>
+          </Container>
+        </Stack>
       </Container>
     </KitchenSinkLayout>
   );
