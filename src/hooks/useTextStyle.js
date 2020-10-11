4c007eea041d7f091accb5ec9de51b063e133549
Misha Moroshko <michael.moroshko@gmail.com>
2020-10-12 08:39:21 +1100

    Remove textStyleCSS from useTextStyle

--- src/hooks/useTextStyle.js
+++ src/hooks/useTextStyle.js
@@ -1,17 +1,14 @@
 import React, { useContext } from "react";
-import useTheme from "./useTheme";
 
 const TextStyleContext = React.createContext();
 
 export const TextStyleProvider = TextStyleContext.Provider;
 
 function useTextStyle() {
-  const theme = useTheme();
   const textStyle = useContext(TextStyleContext);
 
   return {
     textStyle,
-    textStyleCSS: theme.getTextStyleCSS(textStyle),
   };
 }
 
