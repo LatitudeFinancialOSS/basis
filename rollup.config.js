f458c6143734827900242dcb7f05cf0c57cc2fc7
Ali Fathieh <ali.fathieh@gmail.com>
2021-03-26 09:42:03 +1100

    scriptts added

---
+++ rollup.config.js
@@ -0,0 +1,40 @@
+import typescript from "rollup-plugin-typescript2";
+import del from "rollup-plugin-delete";
+import pkg from "./package.json";
+import { babel } from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      //   {
+      //     file: "playground/src/component-lib/index.js",
+      //     format: "esm",
+      //     banner: "/* eslint-disable */",
+      //   },
+      {
+        file: "website/lib/basis-lib/index.js",
+        format: "esm",
+        banner: "/* eslint-disable */",
+      },
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "esm" },
+    ],
+    plugins: [
+      del({
+        targets: [
+          "dist/*",
+          //   "playground/src/component-lib",
+          "website/lib/basis-lib",
+        ],
+      }),
+      babel({ babelHelpers: "bundled" }),
+      commonjs(),
+      typescript({
+        include: ["src/*.js", "src/**/*.js", "src/*.ts(x)", "src/**/*.ts(x)"],
+      }),
+    ],
+    external: Object.keys(pkg.peerDependencies || {}),
+  },
+];
