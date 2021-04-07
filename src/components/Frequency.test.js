dc4eca3c54dc9bb3a866a26b1a6857a8c1d42a57
Strahinja Ajvaz <strahinja.ajvaz@latitudefinancial.com>
2021-04-07 14:07:57 +1000

    added test

--- src/components/Frequency.test.js
+++ src/components/Frequency.test.js
@@ -156,4 +156,16 @@ describe("Frequency", () => {
       "my-frequency"
     );
   });
+
+  it("with amountMaxLength", () => {
+    const { container } = render(
+      <FormWithFrequency
+        label="Salary"
+        testId="my-frequency"
+        amountMaxLength="4"
+      />
+    );
+
+    expect(container.querySelector("input")).toHaveAttribute("maxLength", "4");
+  });
 });
