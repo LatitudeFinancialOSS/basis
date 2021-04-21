a0b68b446cc416bb484908ed9474ae80a4a692f9
Misha Moroshko <michael.moroshko@gmail.com>
2021-04-21 21:13:07 +1000

    Allow RadioGroup option label to be JSX (#212)

--- src/utils/string.js
+++ src/utils/string.js
@@ -1,7 +1,3 @@
 export function pluralize(count, word) {
   return count === 1 ? `1 ${word}` : `${count} ${word}s`;
 }
-
-export function notStringOrEmpty(string) {
-  return typeof string !== "string" || string.trim() === "";
-}
