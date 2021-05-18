669bf903e4b69672601cf5ad6394a696efd49205
Hamza Shahid <hamzashahid321@gmail.com>
2021-05-18 11:34:05 +1000

    Expose the default validation functions from basis

--- src/hooks/useBasisForm/nameToValidateMap.ts
+++ src/hooks/useBasisForm/nameToValidateMap.ts
@@ -1,9 +1,11 @@
 import { ComponentNames } from "../../components/componentNames";
 import { ValidateFn } from "../../types";
-import { validateDateInput } from "./validation/validateDateInput";
-import { validateInput } from "./validation/validateInput";
-import { validateRadioGroup } from "./validation/validateRadioGroup";
-import { validateSelect } from "./validation/validateSelect";
+import {
+  validateDateInput,
+  validateInput,
+  validateRadioGroup,
+  validateSelect,
+} from "./validation";
 
 export const nameToValidateMap: Record<ComponentNames, ValidateFn> = {
   [ComponentNames.Input]: validateInput,
