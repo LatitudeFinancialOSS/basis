4ef2feaf31cceb0a815aca115c617f26680b33c0
Misha Moroshko <michael.moroshko@gmail.com>
2021-06-03 10:40:26 +1000

    Add AppSignal

--- website/src/layouts/Page.js
+++ website/src/layouts/Page.js
@@ -1,5 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
+import Appsignal from "@appsignal/javascript";
+import { ErrorBoundary } from "@appsignal/react";
 import { RecoilRoot } from "recoil";
 import { Global } from "@emotion/react";
 import { Link as GatsbyLink } from "gatsby";
@@ -16,145 +18,151 @@ import { getTabsUrls } from "../utils/url";
 import "typeface-montserrat";
 import "typeface-roboto";
 
+const appsignal = new Appsignal({
+  key: process.env.GATSBY_APPSIGNAL_KEY,
+});
+
 function Page({ pageContext, children, location }) {
   const { header, status, layout = "default" } = pageContext;
   const title = header ? `${header} | Basis` : "Basis";
   const urls = getTabsUrls(location);
 
   return (
-    <RecoilRoot>
-      <BasisProvider theme={theme} InternalLink={GatsbyLink}>
-        <Global
-          styles={{
-            body: {
-              margin: 0,
-              fontFamily: theme.fonts.body,
-              fontSize: theme.fontSizes[1],
-              lineHeight: theme.lineHeights[2],
-              color: theme.colors.black,
-            },
-            a: {
-              color: "inherit",
-              textDecoration: "none",
-            },
-            ":focus": {
-              outline: 0,
-            },
-            '[data-basis-keyboard-mode="true"] :not([aria-invalid="true"]):focus': {
-              boxShadow: `0 0 0px ${theme.radii[1]} ${theme.colors.secondary.lightBlue.t80}`,
-            },
-          }}
-        />
-        <Seo title={title} />
-        <Splitbee />
-        {layout === "empty" ? (
-          <main>{children}</main>
-        ) : (
-          <div
-            css={{
-              height: "100vh",
-              display: "grid",
-              gridTemplateColumns: "224px 1fr",
+    <ErrorBoundary instance={appsignal}>
+      <RecoilRoot>
+        <BasisProvider theme={theme} InternalLink={GatsbyLink}>
+          <Global
+            styles={{
+              body: {
+                margin: 0,
+                fontFamily: theme.fonts.body,
+                fontSize: theme.fontSizes[1],
+                lineHeight: theme.lineHeights[2],
+                color: theme.colors.black,
+              },
+              a: {
+                color: "inherit",
+                textDecoration: "none",
+              },
+              ":focus": {
+                outline: 0,
+              },
+              '[data-basis-keyboard-mode="true"] :not([aria-invalid="true"]):focus': {
+                boxShadow: `0 0 0px ${theme.radii[1]} ${theme.colors.secondary.lightBlue.t80}`,
+              },
             }}
-          >
-            <Sidebar location={location} />
-            <main
+          />
+          <Seo title={title} />
+          <Splitbee />
+          {layout === "empty" ? (
+            <main>{children}</main>
+          ) : (
+            <div
               css={{
-                minHeight: 0,
-                display: "flex",
-                flexDirection: "column",
-                overflow: "auto",
+                height: "100vh",
+                display: "grid",
+                gridTemplateColumns: "224px 1fr",
               }}
             >
-              {header && (
-                <div
-                  css={{
-                    borderBottom: `1px solid ${theme.colors.grey.t16}`,
-                  }}
-                >
-                  <div
-                    css={{
-                      display: "flex",
-                      padding: `${theme.space[5]} ${theme.space[6]} 0`,
-                    }}
-                  >
-                    <Text as="h1" textStyle="heading4">
-                      {header}
-                    </Text>
-                    {status && (
-                      <Container margin="0 0 0 9">
-                        <ComponentStatusIndicator status={status} />
-                      </Container>
-                    )}
-                    <FeedbackFish projectId="42362f272b4510">
-                      <div css={{ marginLeft: "auto" }}>
-                        <Button variant="secondary">Feedback</Button>
-                      </div>
-                    </FeedbackFish>
-                  </div>
-
-                  <ul
-                    css={{
-                      display: "flex",
-                      margin: `${theme.space[6]} 0 0`,
-                      padding: 0,
-                    }}
-                  >
-                    {urls.map(({ name, href, isCurrent }) => (
-                      <li
-                        css={{
-                          listStyleType: "none",
-                          color: isCurrent
-                            ? theme.colors.black
-                            : theme.colors.grey.t65,
-                          ...(isCurrent && {
-                            "::after": {
-                              content: "''",
-                              display: "block",
-                              height: theme.borderWidths[1],
-                              margin: `0 ${theme.space[6]}`,
-                              backgroundColor: theme.colors.black,
-                            },
-                          }),
-                        }}
-                        key={name}
-                      >
-                        <GatsbyLink
-                          css={{
-                            display: "flex",
-                            alignItems: "center",
-                            boxSizing: "border-box",
-                            width: "100%",
-                            padding: `${theme.space[2]} ${theme.space[6]}`,
-                            color: isCurrent
-                              ? theme.colors.black
-                              : theme.colors.grey.t75,
-                          }}
-                          to={href}
-                        >
-                          {name}
-                        </GatsbyLink>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
-              <div
+              <Sidebar location={location} />
+              <main
                 css={{
+                  minHeight: 0,
                   display: "flex",
                   flexDirection: "column",
-                  flexGrow: 1,
-                  minHeight: 0,
-                  overflowY: "auto",
+                  overflow: "auto",
                 }}
               >
-                {children}
-              </div>
-            </main>
-          </div>
-        )}
-      </BasisProvider>
-    </RecoilRoot>
+                {header && (
+                  <div
+                    css={{
+                      borderBottom: `1px solid ${theme.colors.grey.t16}`,
+                    }}
+                  >
+                    <div
+                      css={{
+                        display: "flex",
+                        padding: `${theme.space[5]} ${theme.space[6]} 0`,
+                      }}
+                    >
+                      <Text as="h1" textStyle="heading4">
+                        {header}
+                      </Text>
+                      {status && (
+                        <Container margin="0 0 0 9">
+                          <ComponentStatusIndicator status={status} />
+                        </Container>
+                      )}
+                      <FeedbackFish projectId="42362f272b4510">
+                        <div css={{ marginLeft: "auto" }}>
+                          <Button variant="secondary">Feedback</Button>
+                        </div>
+                      </FeedbackFish>
+                    </div>
+
+                    <ul
+                      css={{
+                        display: "flex",
+                        margin: `${theme.space[6]} 0 0`,
+                        padding: 0,
+                      }}
+                    >
+                      {urls.map(({ name, href, isCurrent }) => (
+                        <li
+                          css={{
+                            listStyleType: "none",
+                            color: isCurrent
+                              ? theme.colors.black
+                              : theme.colors.grey.t65,
+                            ...(isCurrent && {
+                              "::after": {
+                                content: "''",
+                                display: "block",
+                                height: theme.borderWidths[1],
+                                margin: `0 ${theme.space[6]}`,
+                                backgroundColor: theme.colors.black,
+                              },
+                            }),
+                          }}
+                          key={name}
+                        >
+                          <GatsbyLink
+                            css={{
+                              display: "flex",
+                              alignItems: "center",
+                              boxSizing: "border-box",
+                              width: "100%",
+                              padding: `${theme.space[2]} ${theme.space[6]}`,
+                              color: isCurrent
+                                ? theme.colors.black
+                                : theme.colors.grey.t75,
+                            }}
+                            to={href}
+                          >
+                            {name}
+                          </GatsbyLink>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                )}
+                <div
+                  css={{
+                    display: "flex",
+                    flexDirection: "column",
+                    flexGrow: 1,
+                    minHeight: 0,
+                    overflowY: "auto",
+                  }}
+                >
+                  {children}
+                </div>
+              </main>
+            </div>
+          )}
+        </BasisProvider>
+      </RecoilRoot>
+    </ErrorBoundary>
   );
 }
 
